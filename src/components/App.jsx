8a426ae5088ceb795e20411fc6d09209d38d84ff
Ihor Diuzhev <110743288+IhorDiu@users.noreply.github.com>
2023-04-23 00:01:44 +0300

    The layout is done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,31 @@
+import logo from 'img/Logo.png';
+import bg_img from 'img/bg_img.png';
+import avatar from 'img/Hansel.png';
+
+import {
+  Card,
+  Logo,
+  BgImg,
+  Rectangle,
+  Ellipse,
+  Avatar,
+  Tweets,
+  Followers,
+  Button,
+} from './App.styled';
+
 export const App = () => {
   return (
-    <div>
-      Tweet
-    </div>
+    <Card>
+      <Logo src={logo} alt="Logo" />
+      <BgImg src={bg_img} alt="Background" />
+      <Rectangle />
+      <Ellipse>
+        <Avatar src={avatar} alt="Avatar" />
+      </Ellipse>
+      <Tweets>777 tweets</Tweets>
+      <Followers>100,500 Followers</Followers>
+      <Button>Follow</Button>
+    </Card>
   );
 };
