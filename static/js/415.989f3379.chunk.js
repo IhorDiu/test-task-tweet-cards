200b9743c155d8a607ef8756e761728f302869a9
IhorDiu <110743288+IhorDiu@users.noreply.github.com>
2023-04-26 21:01:49 +0000

    Deploying to gh-pages from @ IhorDiu/test-task-tweet-cards@84760028b69b2e435061f67eac64ceed0082b494 🚀

--- static/js/415.989f3379.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunktest_task_tweet_cards=self.webpackChunktest_task_tweet_cards||[]).push([[415],{5415:function(e,t,s){s.r(t);var n=s(184);t.default=function(){return(0,n.jsx)("main",{children:(0,n.jsx)("h1",{children:"Hello world!"})})}}}]);
-//# sourceMappingURL=415.989f3379.chunk.js.map
